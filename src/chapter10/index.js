0eddc287527e988d196a1b3b299003f024d72057
koezay <kozetuo.ofujeapeji@pau.edu.ng>
2023-02-06 12:11:13 +0100

    i dey try

---
+++ src/chapter10/index.js
@@ -0,0 +1,7 @@
+(function(){
+    document.getElementById("output").innerHTML = "Hello Chapter 10"
+})();
+
+// (() => {
+//    document.getElementById("output").innerHTML = "Hello Chapter 10"
+// })(); 
